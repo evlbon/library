d0dbbb45d190b83198f8ff7257c144bcd924ff7d
JafarBadour <jafer.ba@Gmail.com>
2018-02-04 23:28:32 +0300

    Comments on AV.js

--- imports/models/documents/av.js
+++ imports/models/documents/av.js
@@ -1,9 +1,9 @@
-import { Class } from 'meteor/jagi:astronomy';
-import {Document} from "./document";
+import { Class } from 'meteor/jagi:astronomy'; // Importing our extension library astronomy
+import {Document} from "./document";           // Importing our docs
 
 export const AV = Document.inherit({
     name: 'AV',
-    collection: new Meteor.Collection('av'),
+    collection: new Meteor.Collection('av'), // Here to Link out DataBase MongoDB with our Project
 
     // helpers: {
     //

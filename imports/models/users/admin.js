5942eeda99a9754f32566327b3a211c6c21d2d74
Ali <akhmetbek.ali@gmail.com>
2018-04-08 13:01:44 +0300

    Users

---
+++ imports/models/users/admin.js
@@ -0,0 +1,9 @@
+import { Class } from 'meteor/jagi:astronomy';  // Importing extension library astronomy
+import { User } from "./user";                  // Importing users
+
+export const Admin = User.inherit({         // Creating librarian class which is inherited from User
+    name: 'admin',
+    fields: {
+
+    }
+});
